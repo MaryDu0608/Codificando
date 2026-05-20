e32aab810e031ecdf58a620cfd2e0dabcdbd72f8
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-19 21:40:38 -0300

    informações NPC Madelaine

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    
+    public string nome;
+    public bool interagível;
+    public int idade;
 
     void Start()
     {
-        
+        NPC madelaine = new NPC();
+        madelaine.nome = "madelaine";
+        madelaine.idade = 19;
+        madelaine.interagível = true;
     }
 
    
