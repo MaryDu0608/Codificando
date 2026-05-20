eb1e63a3f841562cb90bec47b52ac496b3ff2c3c
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-19 21:31:45 -0300

    Sono adicionado

--- Assets/Scripts/Gatos.cs
+++ Assets/Scripts/Gatos.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Gatos : MonoBehaviour
 {
     public string nome;
+    public bool sono;
 
     private void Start()
     {
         Gatos malhado = new Gatos();
         malhado.nome = "Açucarado";
+        malhado.sono = true;
     }
     
 
