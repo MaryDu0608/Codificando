894a05b566533e49bbf14d119f0226e154a26c69
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-19 21:47:29 -0300

    NPC

---
+++ Assets/Scripts/Inimigos.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Inimigos : MonoBehaviour
+{
+    public string nome;
+    public int dano;
+    public bool boss;
+    public 
+
+    void Start()
+    {
+        
+    }
+
+    
+}
