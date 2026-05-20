48691598aaade51c0643684d97743d694216e4cd
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-20 20:53:46 -0300

    List Inimigo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,17 @@
 using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public List<Inimigo> inimigoList = new();
+
+    private void Start()
+    {
+        for(int i = 0; i < inimigoList.Count; i++)
+        {
+            inimigoList[i].Atacar();
+
+        }
+    }
 }
