752d6517741fe5e9b5bb5cf915fe859f26202ea2
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-19 21:26:17 -0300

    Primeira Classe

---
+++ Assets/Scripts/Gatos.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Gatos : MonoBehaviour
+{
+    public string nome;
+
+    private void Start()
+    {
+        Gatos malhado = new Gatos();
+        malhado.nome = "Açucarado";
+    }
+    
+
+    
+}
