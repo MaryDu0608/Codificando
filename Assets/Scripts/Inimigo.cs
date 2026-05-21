2bceb333c5f25919d531fb973b3ad96b52da5d2c
maria_santos201 <maria_santos201@estudante.sesisenai.org.br>
2026-05-20 21:00:45 -0300

    Ataque especial

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -6,7 +6,13 @@ public class Inimigo : MonoBehaviour
     public int dano;
     public int vida;
     public bool specialAttack;
-    
+
+
+    private void Awake()
+    {
+        dano = Random.Range(1, 21);
+        specialAttack = dano > 10;
+    }
 
     public void Atacar()
     {
